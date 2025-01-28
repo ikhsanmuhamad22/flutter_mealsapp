93495acd0acd66cae9d14a271e7d888fe28ff766
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-01-28 16:23:25 +0700

    add: mealsScreen & fix categoriesScreen

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_categories.dart';
+import 'package:meals_app/widget/categories.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick your categories'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(24),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            Categories(category: category)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/categories.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key, required this.category});
+
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      hoverColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.6),
+            category.color.withOpacity(0.22)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+      ),
+    );
+  }
+}
